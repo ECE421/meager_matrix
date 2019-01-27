48aaa2ccbc243b42b72df1b7d7180085fb3f364b
Thomas Lorincz <tlorincz@ualberta.ca>
2019-01-26 23:20:33 -0700

    SparseMatrix init is now implemented and tested.

--- lib/sparse_matrix.rb
+++ lib/sparse_matrix.rb
@@ -1,65 +1,38 @@
 # Generic Sparse Matrix (CSR storage)
 class SparseMatrix
+  attr_reader(:num_rows, :num_cols, :a_array, :ia_array, :ja_array)
+
   # Basic initialization. Assumes matrix input is properly formatted.
-  def initialize(matrix: Array)
-    @nnz = 0
+  def initialize(matrix)
     @num_rows = matrix.length
     @num_cols = matrix[0].length # TODO: Bad assumption
     @a_array = []
     @ia_array = [0]
     @ja_array = []
 
-    zero_count = 0
     matrix.each do |row|
+      nonzero_count = 0
       if row.is_a?(Array)
         row.each_with_index do |value, index|
           if value.zero?
-            zero_count += 1
+            next
           else
-            @nnz += 1
-            @a_array.insert(@a_array.length, value)
-            @ia_array.insert(@ia_array.length, @ia_array[@ia_array.length - 1] + zero_count)
-            zero_count = 0
-            @ja_array.insert(@ja_array.length, index)
+          nonzero_count += 1
+          @a_array.insert(@a_array.length, value)
+          @ja_array.insert(@ja_array.length, index)
           end
         end
       else
         TypeError
       end
+      @ia_array.insert(@ia_array.length, @ia_array[@ia_array.length - 1] + nonzero_count)
     end
   end
 
   private
 
-  def check_index(row: Integer, col: Integer)
+  def check_index(row, col)
     IndexError if row < 0 || row > @num_rows
     IndexError if col < 0 || col > @num_cols
-  end
-
-  public
-
-  def get(row: Integer, col: Integer)
-    self.check_index(row, col)
-    # TODO
-  end
-
-  def insert(val: Float, row: Integer, col: Integer)
-    self.check_index(row, col)
-    # TODO
-  end
-
-  def remove(row: Integer, col: Integer)
-    self.check_index(row, col)
-    # TODO
-  end
-
-  def dot(other_matrix: SparseMatrix)
-    # TODO
-    self
-  end
-
-  def transpose
-    # TODO
-    self
   end
 end
