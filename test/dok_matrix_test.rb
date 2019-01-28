6184dc705d785393e6d2c32f6caa80a7db3a1a99
Thomas Lorincz <tlorincz@ualberta.ca>
2019-01-28 12:38:23 -0700

    Added test for empty DOK initialize

--- test/dok_matrix_test.rb
+++ test/dok_matrix_test.rb
@@ -21,6 +21,11 @@ class DOKMatrixTest < Test::Unit::TestCase
     assert_equal({ '1,0': 5, '1,1': 8, '2,2': 3, '3,1': 6 }, matrix.dict)
   end
 
+  def test_initialize_no_args
+    matrix = DOKMatrix.new
+    assert_equal({}, matrix.dict)
+  end
+
   def test_power
     matrix = DOKMatrix.new(
       [[0, 0, 0, 0], [5, 8, 0, 0], [0, 0, 3, 0], [0, 6, 0, 0]]
