99a6a1c9b95eb515df1d50adef0161ef723bd321
Thomas Lorincz <tlorincz@ualberta.ca>
2019-01-28 10:32:59 -0700

    Improved init method exception handling

--- lib/sparse_matrix.rb
+++ lib/sparse_matrix.rb
@@ -13,16 +13,14 @@ class SparseMatrix
     matrix = args[0]
     matrix.each do |row|
       nonzero_count = 0
-      if row.is_a?(Array)
-        row.each_with_index do |value, index|
-          next unless value.nonzero?
+      raise(TypeError) unless row.is_a?(Array)
 
-          nonzero_count += 1
-          @a_array.insert(@a_array.length, value)
-          @ja_array.insert(@ja_array.length, index)
-        end
-      else
-        TypeError
+      row.each_with_index do |value, index|
+        next unless value.nonzero?
+
+        nonzero_count += 1
+        @a_array.insert(@a_array.length, value)
+        @ja_array.insert(@ja_array.length, index)
       end
       @ia_array.insert(@ia_array.length,
                        @ia_array[@ia_array.length - 1] + nonzero_count)
