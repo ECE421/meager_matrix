f7b21d0adaa94461b4806d45f6582c2bab292028
Nathan Klapstein <nklapste@ualberta.ca>
2019-01-28 19:09:15 -0700

    adding basic `matrix` class

---
+++ lib/matrix.rb
@@ -0,0 +1,41 @@
+class Matrix
+  def to_array
+    raise NotImplementedError,
+          'Matrix is an abstract class please implement methods'
+  end
+
+  def cross
+    raise NotImplementedError,
+          'Matrix is an abstract class please implement methods'
+  end
+
+  def determinant
+    raise NotImplementedError,
+          'Matrix is an abstract class please implement methods'
+  end
+
+  def dot
+    raise NotImplementedError,
+          'Matrix is an abstract class please implement methods'
+  end
+
+  def multiply
+    raise NotImplementedError,
+          'Matrix is an abstract class please implement methods'
+  end
+
+  def power
+    raise NotImplementedError,
+          'Matrix is an abstract class please implement methods'
+  end
+
+  def trace
+    raise NotImplementedError,
+          'Matrix is an abstract class please implement methods'
+  end
+
+  def transpose
+    raise NotImplementedError,
+          'Matrix is an abstract class please implement methods'
+  end
+end
