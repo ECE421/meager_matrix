d7576443dfbf7cba635cc9fa98c9df34810de8e7
zdrever <zdrever@ualberta.ca>
2019-01-29 11:37:36 -0700

    [fix] TestMatrixGenerator formatting

--- test/sparse_matrix_generator.rb
+++ test/sparse_matrix_generator.rb
@@ -1,22 +1,21 @@
 class TestMatrixGenerator
-
-  def self.generate_sparse_matrix(rows, cols, ratio:0.5, rand_max:10)
-    # Generate a random general sparse matrix with non-zero element ratio < ratio
+  def self.generate_sparse_matrix(rows, cols,
+      ratio: 0.5, rand_max: 10)
+    # Generate a random general sparse matrix with non-zero element
+    # ratio < ratio
     arr = Array.new(rows, Array.new(cols, 0))
-    arr.map!{|row| row.map{ rand < ratio ? rand(rand_max) : 0}}
-
-    return arr
+    arr.map! { |row| row.map{ rand < ratio ? rand(rand_max) : 0 } }
   end
 
-  def self.generator_diag_matrix(rows, cols, band_width:1, band_offset:0, rand_max:10)
+  def self.generator_diag_matrix(rows, cols,
+      band_width: 1, band_offset: 0, rand_max: 10)
     # Generate random band matrices
     arr = []
     rows.times do |n|
       s = n + band_offset > 0 ? n + band_offset : 0
       e = n + band_offset + band_width > 0 ? n + band_offset + band_width : 0
-      arr << Array.new(cols, 0).map!.with_index {|elem, i| (i >= s && i < e) ? rand(rand_max): 0}
+      arr << Array.new(cols, 0).map!.with_index { |elem, i|
+        i >= s && i < e ? rand(rand_max) : 0 }
     end
-    return arr
   end
-
 end
