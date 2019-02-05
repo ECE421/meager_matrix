e97aa4bdfdad7ae71de1fb9d438f470f82d8412b
Nathan Klapstein <nklapste@ualberta.ca>
2019-02-05 13:32:37 -0700

    rebuilding tests for `DiagonalMatrix`

--- lib/diagonal_matrix.rb
+++ lib/diagonal_matrix.rb
@@ -6,38 +6,44 @@ require_relative '../lib/sparse_matrix'
 class DiagonalMatrix < SparseMatrix
   attr_reader(:diagonal)
 
+  def initialize(diagonal, row_count, column_count)
+    @diagonal = diagonal
+    @row_count = row_count
+    @column_count = column_count
+  end
+
   # Initialize a diagonal sparse matrix from a array based definition
   # Note: non diagonal values will be dropped
-  def initialize(*args)
-    @diagonal = []
-    return unless args.length.nonzero?
-    raise(TypeError) unless args[0].is_a?(Array)
+  def self.rows(rows, copy = true)
+    diagonal = []
+    raise(TypeError) unless rows.is_a?(Array)
 
-    matrix = args[0]
-    @num_row = matrix.length
-    matrix.each_with_index do |row, i|
+    num_col = 0
+    num_row = rows.length
+    rows.each_with_index do |row, i|
       raise(TypeError) unless row.is_a?(Array)
 
-      @num_col = row.length
+      num_col = row.length
 
-      next if i >= @num_col
+      next if i >= num_col
 
-      @diagonal.push(row[i])
+      diagonal.push(row[i])
     end
+    new diagonal, num_row, num_col
   end
 
   # return the DiagonalMatrix as a @num_row long Array of @num_col long Arrays
   def to_matrix
     i = 0
-    matrix = Array.new(@num_row, Array.new(@num_col, 0))
+    matrix = Array.new(@row_count, Array.new(@column_count, 0))
     while i < @diagonal.length
-      row = Array.new(@num_col, 0)
+      row = Array.new(@column_count, 0)
       row[i] = diagonal.at(i)
       matrix[i] = row
       puts(matrix)
       i += 1
     end
-    matrix
+    Matrix.rows(matrix)
   end
 
   def power(exponent)
--- test/diagonal_matrix_test.rb
+++ test/diagonal_matrix_test.rb
@@ -17,21 +17,34 @@ class DiagonalMatrixTest < Test::Unit::TestCase
   # Test the matrix initialization code
   # Based on https://en.wikipedia.org/wiki/Sparse_matrix#Compressed_sparse_row_(CSR,_CRS_or_Yale_format)
   def test_initialization
-    assert_equal([0, 0], DiagonalMatrix.new([[0, 0], [0, 0]]).diagonal)
+    assert_equal([0, 0], DiagonalMatrix.rows([[0, 0], [0, 0]]).diagonal)
   end
 
-  def test_initialization_empty
-    assert_equal([], DiagonalMatrix.new([]).diagonal)
+  def test_rows
+    diagonal_matrix = DiagonalMatrix.rows([[0, 0], [0, 0]])
+    assert_true(diagonal_matrix.is_a?DiagonalMatrix)
+    assert_true(diagonal_matrix.is_a?SparseMatrix)
+    assert_true(diagonal_matrix.is_a?Matrix)
+  end
+
+  def test_to_matrix
+    diagonal_matrix = DiagonalMatrix.rows([[0, 0], [0, 0]])
+    matrix = diagonal_matrix.to_matrix
+    assert_false(matrix.is_a?DiagonalMatrix)
+    assert_false(matrix.is_a?SparseMatrix)
+    assert_true(matrix.is_a?Matrix)
+    assert_equal([[0, 0], [0, 0]], matrix.to_a)
   end
 
   def test_extra_row_matrix
-    matrix = DiagonalMatrix.new([[1, 0], [0, 2], [0, 0]])
+    matrix = DiagonalMatrix.rows([[1, 0], [0, 2], [0, 0]])
+
     assert_equal([[1, 0], [0, 2], [0, 0]], matrix.to_matrix)
     assert_equal([1, 2], matrix.diagonal)
   end
 
   def test_extra_col_matrix
-    matrix = DiagonalMatrix.new([[1, 0, 0], [0, 2, 0]])
+    matrix = DiagonalMatrix.rows([[1, 0, 0], [0, 2, 0]])
     assert_equal([[1, 0, 0], [0, 2, 0]], matrix.to_matrix)
     assert_equal([1, 2], matrix.diagonal)
   end
