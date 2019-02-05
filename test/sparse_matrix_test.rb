ff08e34ba1f9ab92246fb88be0c1961c69058d8a
Nathan Klapstein <nklapste@ualberta.ca>
2019-02-05 13:08:58 -0700

    removing comment

--- test/sparse_matrix_test.rb
+++ test/sparse_matrix_test.rb
@@ -2,15 +2,9 @@ require 'test/unit'
 require_relative '../lib/sparse_matrix'
 
 class SparseMatrixTest < Test::Unit::TestCase
-
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
   def setup
     # Do nothing
   end
-
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
 
   def teardown
     # Do nothing
