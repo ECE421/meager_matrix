80d22bcf06a7d94214f5841677ea477ac002415d
Thomas Lorincz <tlorincz@ualberta.ca>
2019-02-05 13:17:05 -0700

    Base work for SparseMatrixFactory

---
+++ lib/csr_matrix_factory.rb
@@ -0,0 +1,10 @@
+# Concrete Factory for CSRMatrix
+class CSRMatrixFactory < SparseMatrixFactory
+  def build_from_matrix(source_matrix)
+    # TODO
+  end
+
+  def build_from_array(source_array)
+    # TODO
+  end
+end
---
+++ lib/sparse_matrix_factory.rb
@@ -0,0 +1,20 @@
+# Abstract Factory Superclass
+class SparseMatrixFactory
+  def self.build(source, type = CSRMatrixFactory.new)
+    raise TypeError unless type.is_a?(SparseMatrixFactory)
+
+    if source.is_a?(Matrix)
+      type.build_from_matrix(source)
+    elsif source.is_a?(Array)
+      type.build_from_array(source)
+    else
+      throw(TypeError)
+    end
+  end
+
+  protected
+
+  def build_from_matrix(source_matrix) end
+
+  def build_from_array(source_array) end
+end
