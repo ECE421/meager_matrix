241b8ec10e08541f1c3d58b72c4eb71493f0bae5
Nathan Klapstein <nklapste@ualberta.ca>
2019-02-05 13:47:27 -0700

    removing unneeded comments

--- test/diagonal_matrix_test.rb
+++ test/diagonal_matrix_test.rb
@@ -2,14 +2,11 @@ require 'test/unit'
 require_relative '../lib/diagonal_matrix'
 
 class DiagonalMatrixTest < Test::Unit::TestCase
-  # Called before every test method runs.
-  # Can be used to set up fixture information.
+
   def setup
     # Do nothing
   end
 
-  # Called after every test method runs.
-  # Can be used to tear down fixture information.
   def teardown
     # Do nothing
   end
