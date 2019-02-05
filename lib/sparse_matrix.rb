f4f27087cef368ca0ab8f17a1f8ec6c41491c57a
Nathan Klapstein <nklapste@ualberta.ca>
2019-02-05 12:52:42 -0700

    adding `rows` stub

--- lib/sparse_matrix.rb
+++ lib/sparse_matrix.rb
@@ -2,6 +2,8 @@ require 'matrix'
 
 # Abstract class definition of a sparse matrix
 class SparseMatrix < Matrix
-
-
+  def rows(rows, copy = true)
+    raise NotImplementedError,
+          'SparseMatrix is an abstract class please implement methods'
+  end
 end
