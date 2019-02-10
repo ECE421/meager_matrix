f11dd0897e3ade43d74b05539864c1ef377c6cbe
Thomas Lorincz <tlorincz@ualberta.ca>
2019-02-10 14:29:16 -0700

    Added factory and subclass method stubs

---
+++ lib/tridiagonal_matrix.rb
@@ -0,0 +1,14 @@
+# Tridiagonal sparse matrix
+class TridiagonalMatrix
+  attr_reader(:row_count, :column_count)
+
+  # Basic initialization. Assumes matrix input is properly formatted.
+  def initialize(row_count, column_count)
+    @row_count = row_count
+    @column_count = column_count
+  end
+
+  def self.rows(rows, column_count = rows[0].length)
+    # TODO: Implement
+  end
+end
